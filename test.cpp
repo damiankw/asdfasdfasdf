09af38589987539b5559bdf505b7eaf24330d056
damian <damian@damian.id.au>
2017-10-10 20:12:26 +1100

    Added variables.

--- test.cpp
+++ test.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main() {
+  // basic output
   std::cout << "Hello, world!\n";
   std::cout << "This is my test application\n";
+  
+  // set up some variables
+  int i = 23;
+  float f = 1.232;
+  char c = 'a';
+  string s = "My string!";
+  
+  // output some variables
+  std::cout << "Int: " << i << endl;
+  std::cout << "Float: " << f << endl;
+  std::cout << "Char: " << c << endl;
+  std::cout << "String: " << s << endl;
 }
